4ff29fcc84293216aedbab114a79acb1384931cb
smartao <sergei.martao@gmail.com>
2018-09-03 15:31:28 -0300

    Atualizado o README

--- desliga_instancias/desliga_instancias.sh
+++ desliga_instancias/desliga_instancias.sh
@@ -1,21 +1,4 @@
 #!/bin/bash
-# Versao: 2018.05.28
-# Descricao: Script de automacao de ativacao e desativacao de Instancias alocadas no OpenStack.
-# Regra: Criar o nome da instancia com a descrição do horário que deseja ligar e desligar.
-#        Lembrando que o nome da instancia nao pode ultrapassar 64 caracteres.
-# Parametros 
-#       WA = Walkup Ligando as instancias
-#       ST = Stop   Desligando as instancias
-#       Horario de 0 a 23 segundo de h (hora)
-# Exemplos de nomes
-#       srvxpto-WA8h-ST18h  (Para ligar as 8 da manha e desligar as 18h)
-#       srvacme-WA10h-ST20h (Para ligar as 10h da manhã e desligar as 20h)     
-#       servidor123ab-ST23h       (Apenas desligar as instancias todo dia as 23h)
-# Observações
-#       O script não difere dias da semana, isso fica de responsabilidade do CRON!
-# Exemplo crontab 
-#       0 * * * 1-5 /usr/local/bin/openstack-automacao.sh > /dev/null
-#       1-5 = sera executado apenas de segunda a sexta
 
 ARQ=/tmp/instancias     # Arquivo temporario para armazenar instancias
 PESQUISA[0]=ST`date "+%H"`h # ST = Stop   Desligando as instancias
