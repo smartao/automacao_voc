ad1c5223ef007a5caf5bd1eadf93c76453260e4c
smartao <sergei.martao@gmail.com>
2018-09-17 18:15:01 -0300

    criando pasta para inicio

---
+++ inicio/README.md
@@ -0,0 +1,6 @@
+# Arquivos para iniciar a instalacao do ambiente
+
+Arquivo
+instalando-openstackcli.sh
+
+Responsavel por instalar o openstack-cli 
---
+++ inicio/instalando-openstackcli.sh
@@ -0,0 +1,9 @@
+
+apt-get update
+apt-get upgrade 
+apt-get install python -y
+apt-get install python-setuptools -y
+apt-get install python-pip -y
+apt-get install python-dev -y
+apt install python-openstackclient -y
+pip install python-openstackclient
