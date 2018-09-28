02604329de5ae3540462e779d2b05e6788cecec3
smartao <sergei.martao@gmail.com>
2018-09-28 18:55:51 -0300

    Desligando por dia da semana

--- desliga_instancias/desliga_instancias.sh
+++ desliga_instancias/desliga_instancias.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 ARQ=/tmp/instancias     # Arquivo temporario para armazenar instancias
-PESQUISA[0]=ST`date "+%H"`h # ST = Stop   Desligando as instancias
-PESQUISA[1]=WA`date "+%H"`h # WA = Walkup Ligando as instancias
-PARAMETRO[0]=stop       # Comando para parar as instancias
-PARAMETRO[1]=start      # Comando para iniciar as instancias
 STATUS[0]=ACTIVE        # Status das instancias ativas, sera usando quando corresponder ao horario das instancias serem desligadas
 STATUS[1]=SHUTOFF       # Status das instancias desalocadas, sera usando quando corresponder ao horario das instancias serem ligadas
+DOW="\-D.+`date "+%w"`" # Day of Week      Dia da Semana de 0 a 6
+PESQUISA[0]=ST`date "+%H"`h # ST = Stop    Desligando as instancias
+PESQUISA[1]=WA`date "+%H"`h # WA = Walkup  Ligando as instancias
+PARAMETRO[0]=stop       # Comando para parar as instancias
+PARAMETRO[1]=start      # Comando para iniciar as instancias
+
 KEYSTONE=/etc/scripts/keystone_vivo_cloud # Localizazao do arquivo de keystone 
 
 source $KEYSTONE # Carregando as variaveis do keystone
@@ -14,7 +16,7 @@ source $KEYSTONE # Carregando as variaveis do keystone
 for ((s=0;s<${#STATUS[@]};s++)); # For que corre os dois status disponveis ACTIVE e SHUTOFF
 do
         # Coletando todos os ID das instancias que corresponde a acao e horario atual
-        openstack server list | cut -d'|' -f 1-4 | grep -i ${STATUS[$s]} | grep ${PESQUISA[$s]} | awk '{print $2}' > $ARQ
+        openstack server list | cut -d'|' -f 1-4 | grep -i ${STATUS[$s]} | grep -E $DOW | grep ${PESQUISA[$s]} | awk '{print $2}' > $ARQ
         # Executuando a ação correspondente do horario programado naquele instancia
         for i in `cat $ARQ`; do openstack server ${PARAMETRO[$s]} $i; done
 done  
