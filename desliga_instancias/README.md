69e02f0d008a4a26332ee7274bc665cf1e655f1a
smartao <sergei.martao@gmail.com>
2018-10-03 15:53:14 -0300

    Configurado agendamento por minuto

--- desliga_instancias/README.md
+++ desliga_instancias/README.md
@@ -8,18 +8,20 @@
  
 #### Parametros:
    
-    WA = Walkup Ligando as instancias
-    ST = Stop   Desligando as instancias
-    Horario de 0 a 23 segundo de h (hora)
-        Deve ter sempre 2 digitor ex, 01, 02 ate 23.
-    D- = Dias da semana de 0 (Domingo) a 6 (Sabado)
+    WA = Walkup Ligando as instancias.
+    ST = Stop   Desligando as instancias.
+    Horario de 0 a 23 seguido do h (hora).
+        Deve ser sempre 2 digitos ex: 01, 02 ate 22 e 23.
+    Minuto de 0 a 55 seguido do m (minuto).
+        Deve ser sempre 2 digitos ex: 00, 05 ate 50 e 55.
+    D- = Dias da semana de 0 (Domingo) a 6 (Sabado).
  
 #### Exemplos:
  
-    srvxpto-WA08h-ST18h-D-0123456 (Ligando as 8 da manha e desligando as 18h todos os dias)
-    srvacme-WA10h-ST20h-D-12345 (Ligando as 10h da manhã e desligando as 20h, de segunda a sexta)
-    servidor123ab-ST23h-D-5 (Apenas desligar as instancias as 23h de sexta-feira)
+    srvxpto-WA08h10m-ST18h30m-D-0123456 (Ligando as 8:10 da manha e desligando as 18:30 todos os dias)
+    srvacme-WA10h00m-ST20h00m-D-12345 (Ligando as 10h da manhã e desligando as 20h, de segunda a sexta)
+    servidor123ab-ST23h45h-D-5 (Apenas desligar as instancias as 23:45 de sexta-feira)
   
 #### Crontab 
-Exemplo de configuração do cron
-    `0 * * * * /usr/local/bin/openstack-automacao.sh > /dev/null`
+Exemplo de configuração para executar o cron a cada 5 minutos
+    `*/5 * * * * /usr/local/bin/openstack-automacao.sh > /dev/null`
--- desliga_instancias/desliga_instancias.sh
+++ desliga_instancias/desliga_instancias.sh
@@ -4,8 +4,9 @@ ARQ=/tmp/instancias     # Arquivo temporario para armazenar instancias
 STATUS[0]=ACTIVE        # Status das instancias ativas, sera usando quando corresponder ao horario das instancias serem desligadas
 STATUS[1]=SHUTOFF       # Status das instancias desalocadas, sera usando quando corresponder ao horario das instancias serem ligadas
 DOW="\-D.+`date "+%w"`" # Day of Week      Dia da Semana de 0 a 6
-PESQUISA[0]=ST`date "+%H"`h # ST = Stop    Desligando as instancias
-PESQUISA[1]=WA`date "+%H"`h # WA = Walkup  Ligando as instancias
+PESQUISAH[0]=ST`date "+%H"`h # ST = Stop    Desligando as instancias
+PESQUISAH[1]=WA`date "+%H"`h # WA = Walkup  Ligando as instancias
+PESQUISAM=h`date "+%M"`m   # Minuto para ativar e destivar o host
 PARAMETRO[0]=stop       # Comando para parar as instancias
 PARAMETRO[1]=start      # Comando para iniciar as instancias
 
@@ -16,7 +17,7 @@ source $KEYSTONE # Carregando as variaveis do keystone
 for ((s=0;s<${#STATUS[@]};s++)); # For que corre os dois status disponveis ACTIVE e SHUTOFF
 do
         # Coletando todos os ID das instancias que corresponde a acao e horario atual
-        openstack server list | cut -d'|' -f 1-4 | grep -i ${STATUS[$s]} | grep -E $DOW | grep ${PESQUISA[$s]} | awk '{print $2}' > $ARQ
+        openstack server list | cut -d'|' -f 1-4 | grep -i ${STATUS[$s]} | grep -E $DOW | grep ${PESQUISAH[$s]} | grep $PESQUISAM | awk '{print $2}' > $ARQ
         # Loop que executara as acoes de ativar, destivar e logar 
         for i in `cat $ARQ`; do 
             # Executuando a ação correspondente do horario programado naquele instancia
