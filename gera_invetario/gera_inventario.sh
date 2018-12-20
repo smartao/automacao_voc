7ab08a3aabefdc582fd5e61b02e4984699a4480f
smartao <sergei.martao@gmail.com>
2018-12-20 15:08:09 -0200

    atualizado gera_inventario

--- gera_invetario/gera_inventario.sh
+++ gera_invetario/gera_inventario.sh
@@ -13,20 +13,17 @@ openstack server list | grep -i $PREFIXO |cut -d"|" -f 3,5 | cut -d\= -f 2 | cut
 echo "[all]"> $DEST/$ARQS
 for i in `cat $DEST/$ARQT`;do echo $i:$PORTA >> $DEST/$ARQS; done
 
-# Coletando o ID de todas as instancias
-openstack server list | grep -i $PREFIXO | awk {'print $2'} > $DEST/$ARQT
+# Filtrando para o hostname instancias
+openstack server list | grep -i $PREFIXO | cut -d"|" -f 3 | sed 's/ //g' > $TEMP/vochostname
 
-# Limpar arquivo de inventario full
-> $DEST/$ARQF
+# Filtrando o primeiro IP de todas as intancias
+openstack server list | grep -i $PREFIXO | cut -d"|" -f 5 | cut -d\= -f 2 | cut -d, -f1 | sed 's/ //g' > $TEMP/vocip
 
-# Gerando lista com hostname e ips
-for i in `cat $DEST/$ARQT`;do
-    # Coletando hostname e IP
-    HNAME=`openstack server show $i | grep " name " | awk {'print $4'}`
-       IP=`openstack server show $i | grep -i address | awk {'print $4'} | cut -d\= -f 2 | cut -d, -f1`
-    # Gerando arquivo com HOSTNAME = IP
-    echo "$HNAME=$IP:$PORTA" >> $DEST/$ARQF
-done
+# Filtro para status
+openstack server list | grep -i $PREFIXO | cut -d"|" -f 3,4 | cut -d"|" -f2 | sed 's/ //g' > $TEMP/vocstatus
+
+# Justando os arquivos
+paste -d";" $TEMP/vochostname $TEMP/vocip $TEMP/vocstatus > $DEST/${ARQF}.csv
 
 # Deletando arquivo temporario
 rm $DEST/$ARQT >/dev/null 1>&2
