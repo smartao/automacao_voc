16dc2367cf862db872976e8ef4ecc4b2aac97ee8
smartao <sergei.martao@gmail.com>
2018-12-31 12:29:40 -0200

    corrigir log do sistema

--- api/stop-start.sh
+++ api/stop-start.sh
@@ -5,7 +5,6 @@ source /usr/local/bin/automacao_voc/variaveis
 
 PASTA[0]="start"       # Pasta que conterá as instancias que deverao ser ligadas
 PASTA[1]="stop"        # Pasta que conterá as instancias que deverao ser desligadas
-
 PARAMETRO[0]="start"    # Comando para iniciar as instancias
 PARAMETRO[1]="stop"     # Comando para parar as instancias
 
@@ -18,8 +17,8 @@ do
     chown $USUARIO:$USUARIO $DEST/${PASTA[$s]} 2>/dev/null # Fornecendo permissos a pasta de gravacao
     for i in `ls $DEST/${PASTA[$s]}`; do
         openstack server ${PARAMETRO[$s]} $i #2>/dev/null # Ligando/desligando instancias que estao na pasta
-        logger -i -t voc "Executado acao ${PARAMETRO[$s]} no servidor $i" # Logando a acao que esta sendo executado
+        logger -i -t voc "executando: openstack server ${PARAMETRO[$s]} $i" # Logando a acao que esta sendo executado
         rm "$DEST/${PASTA[$s]}/$i" 2>/dev/null # Deletando arquivos das instancias para nao ser executado novamente
-        logger -i -t voc "Deletado o arquivo $DEST/${PASTA[$s]}/$i" # Logando a delecao do arquivo 
+        logger -i -t voc "deletado o arquivo $DEST/${PASTA[$s]}/$i" # Logando a delecao do arquivo 
     done
 done
